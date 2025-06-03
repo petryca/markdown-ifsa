39efc6b5adb9de3b0d0b87a601eb5712cc87dbff
David Petryca <david@petryca.com>
2025-06-03 11:53:37 +0200

    update

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const app = Vue.createApp({
     },
     computed: {
         charCount() {
-            return this.message.length;
+            const stripped = this.message.replace(/<[^>]*>/g, '');
+            return stripped.length;
+        },
+        htmlOutput() {
+            return marked.parse(this.message);
+        }
+    },
+    mounted() {
+        const saved = localStorage.getItem('markdownText');
+        if (saved) this.message = saved;
+    },
+    watch: {
+        message(newMessage) {
+            localStorage.setItem('markdownText', newMessage);
         }
     }
 });
